4f4c090718ae602bdf02e36e11fe011277f1b91f
Donatron <don.macarthur68@gmail.com>
2019-06-03 14:26:38 +1000

    nothing

--- src/config/config_prod.js
+++ src/config/config_prod.js
@@ -2,7 +2,8 @@ module.exports = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   APIKey: process.env.API_KEY,
-  imageRootURL: process.env.IMAGE_ROOT_URL
+  imageRootURL: process.env.IMAGE_ROOT_URL,
+  someVariable: "Some Variable"
 };
 
 console.log("Production config");
