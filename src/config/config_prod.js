c3a915006833868e81a634867f0af907368e9a6a
Donatron <don.macarthur68@gmail.com>
2019-06-03 14:34:59 +1000

    push to heroku

--- src/config/config_prod.js
+++ src/config/config_prod.js
@@ -2,8 +2,7 @@ module.exports = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   APIKey: process.env.API_KEY,
-  imageRootURL: process.env.IMAGE_ROOT_URL,
-  someVariable: "Some Variable"
+  imageRootURL: process.env.IMAGE_ROOT_URL
 };
 
 console.log("Production config");
